0668710c79931721013392e9bd57f51f9cd206a3
Eli <me@eliellis.com>
2013-08-10 21:49:24 -0400

    added --edit option to install script preemptively edit configs and rules

--- install.sh
+++ install.sh
@@ -3,20 +3,35 @@ IPTABLES_DIR="/etc/iptables"
 INIT_DIR="/etc/init.d/"
 DEFAULT_DIR="/etc/default/"
 
-echo "Building warp drive..."
 cp "iptables-persistent" $INIT_DIR
+
+if [[ $1 = "--edit" ]]
+then 
+	nano "iptables-persistent.conf"
+	nano "rules"
+	nano "ipv6_rules"
+fi
+
+echo "Building warp drive..."
+
 cp "iptables-persistent.conf" $DEFAULT_DIR
-if [ -d $IPTABLES_DIR ]
+
+if [[ -d $IPTABLES_DIR ]]
 then
 	echo "\"$IPTABLES_DIR\" doesn't exist, making it"
 	mkdir $IPTABLES_DIR
 fi
+
 cp "rules" $IPTABLES_DIR
 cp "ipv6_rules" $IPTABLES_DIR
+
 echo "Aligned all proton magnets!"
 
+
 echo "Adding fuel to the thrusters..."
+
 update-rc.d iptables-persistent defaults
+
 echo "Everything is all done!"
 
 
