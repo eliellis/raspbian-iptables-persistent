180adbeafe30b876224d9c72db4980ba215e1807
Eli <me@eliellis.com>
2013-08-10 21:18:28 -0400

    we good

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-IPTABLES_DIR="/etc/iptables"
+IPTABLES_DIR=/etc/iptables
 echo "Copying things over..."
 cp "iptables-persistent" "/etc/init.d/"
 cp "iptables-persistent.conf" "/etc/default/"
 if [ -d "$IPTABLES_DIR" ]
 then
-	echo "\"/etc/iptables\" doesn't exist, making it"
-	mkdir "/etc/iptables"
+	echo "\"$IPTABLES_DIR\" doesn't exist, making it"
+	mkdir "$IPTABLES_DIR"
 fi
-cp "rules" "/etc/iptables/"
-cp "ipv6_rules" "/etc/iptables/"
+cp "rules" "$IPTABLES_DIR"
+cp "ipv6_rules" "$IPTABLES_DIR"
 echo "There you go!"
 
 exit 0
